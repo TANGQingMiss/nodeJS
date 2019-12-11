2944774a8b48214e720052a3d058daa3e3ebc3cc
TANG Qing <tangqingmiss@163.com>
2019-12-11 12:46:31 +0800

    Initial commit

---
+++ console.js
@@ -0,0 +1,3 @@
+//two same methods for log info
+console.log("Node.js - console.log() method");
+console.info("Node.js - console.info() method");
---
+++ formatOutput.js
@@ -0,0 +1,3 @@
+//format string output
+console.log("%s","This is a format string program");
+console.log("%s%s%s","you can","connect","several strings");
